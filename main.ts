bffd254ea45982e6e546ac8c13d8d97e9ae466ac
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-01 12:59:07 -0400

    srrtjr

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+ /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Kyle Lyver
  * Created on: Oct 2023 
- * This program ...
+ * This program counts down with neopixels
 */
 
 // variables
@@ -21,47 +21,20 @@ basic.showIcon(IconNames.Happy)
 
 // run loop
 input.onButtonPressed(Button.A, function () {
-  loopCounter = 0
+  loopCounter = 4
   basic.clearScreen()
-  while (loopCounter <= 5) {
-    basic.showNumber(4)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Purple))
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.White))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.White))
+  
+  // 4 neopixels
+  while (loopCounter >= 0) {
+    basic.showNumber(loopCounter)
     neopixelStrip.show()
     pause(1000)
-    basic.clearScreen()
-    basic.showNumber(3)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    pause(1000)
-    basic.clearScreen()
-    basic.showNumber(2)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    pause(1000)
-    basic.clearScreen()
-    basic.showNumber(1)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    pause(1000)
-    basic.clearScreen()
-    basic.showNumber(0)
-    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
-    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-    neopixelStrip.show()
-    loopCounter = loopCounter + 1
+    
+    loopCounter = loopCounter - 1
+    neopixelStrip.setPixelColor(loopCounter, neopixel.colors(NeoPixelColors.Black))
     }
 })
