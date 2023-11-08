c17d53047f48a4dd54b537650d219a35f5b6b761
Kyle Lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-11-08 18:28:37 +0000

    dgnhet

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,15 +1,15 @@
-"""
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
-"""
-
 from microbit import *
 import neopixel
 
-np = neopixel.NeoPixel(pin16, 5)
+# Variables
+num_pixels = 12
+np = neopixel.NeoPixel(pin16, 4)
 loopcounter = 4
 
+# set up
+display.clear()
+display.show(Image.HAPPY)
+sleep(500)
 display.clear()
 np[0] = (0, 0, 0)
 np[1] = (0, 0, 0)
@@ -17,22 +17,31 @@ np[2] = (0, 0, 0)
 np[3] = (0, 0, 0)
 np.show()
 display.show(Image.HAPPY)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
+np[3] = (0, 0, 0)
+print(np[0])
+print(np[1])
+print(np[2])
+print(np[3])
+np.show()
 
-while True():
+# loop
+while True:
     if button_a.is_pressed():
+        loopcounter = 4
+        display.clear()
         np[0] = (255, 0, 0)
         np[1] = (255, 0, 0)
         np[2] = (255, 0, 0)
         np[3] = (255, 0, 0)
-        display.clear()
-        display.show(loopcounter)
-        np[loopcounter] = (0, 0, 0)
-        np.show()
-        sleep(500)
-# loopcounter
-while loopcounter >= 0:
-    display.show(loopcounter)
-    sleep(1000)
-    np[loopcounter] = (0, 0, 0)
-    np.show()
-    loopcounter = loopcounter - 1
+        while loopcounter >= 0:
+            display.show(loopcounter)
+            np.show()
+            sleep(500)
+            loopcounter = loopcounter - 1
+            np[loopcounter] = (0, 0, 0)
+            np.show()
+            sleep(500)
+    display.show(Image.HAPPY)
